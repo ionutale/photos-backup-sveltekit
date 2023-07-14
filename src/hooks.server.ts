e92fdb7fd87e60ead98d2da6b66e089175b76f53
Ion Utale <utale.ion@gmail.com>
2023-07-14 18:26:50 +0200

    added mongodb hook and package

---
+++ src/hooks.server.ts
@@ -0,0 +1,23 @@
+import type { Handle } from '@sveltejs/kit';
+import { MONGO_URI } from '$env/static/private'
+import { MongoClient } from "mongodb";
+import type { Db } from "mongodb"
+
+// Connection URI
+const uri = MONGO_URI
+const client = new MongoClient(uri!);
+
+async function establishConnection(): Promise<Db> {
+  try {
+    await client.connect();
+    return client.db("photos-app");
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+}
+
+export const handle: Handle = async ({ event, resolve }) => {
+  event.locals.db = await establishConnection();
+  return await resolve(event);
+};
