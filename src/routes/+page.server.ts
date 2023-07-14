df86647a61052fbbc02c81d9aeb0be4ec6040b84
Ion Utale <utale.ion@gmail.com>
2023-07-14 22:15:24 +0200

    update images to bucket

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,47 @@
+import { fail } from '@sveltejs/kit';
+import { writeFileSync } from 'fs';
+import { Storage } from '@google-cloud/storage';
+const bucketName = 'aiu-family-media';
+
+export const actions = {
+  login: async (event) => {
+    const formData = Object.fromEntries(await event.request.formData());
+
+    if (
+      !(formData.photos as File).name ||
+      (formData.photos as File).name === 'undefined'
+    ) {
+      return fail(400, {
+        error: true,
+        message: 'You must provide a file to upload'
+      });
+    }
+
+    const { photos } = formData as { photos: File };
+
+    // Write the file to the static folder
+    await loadFileTestFile(photos);
+
+    return {
+      success: true
+    };
+
+  },
+  register: async (event) => {
+    // TODO register the user
+    console.log(event.body);
+  }
+};
+
+async function loadFileTestFile(file: File) {
+  // Creates a client
+  const storage = new Storage();
+  // Uploads a local file to the bucket
+  const fileContents = await file.arrayBuffer();
+  const newBuffer = Buffer.from(fileContents);
+  await storage.bucket(bucketName).file(file.name).save(
+    newBuffer,
+    {}).catch(console.error);
+
+};
+
---
+++ src/routes/api/+server.ts
@@ -0,0 +1,23 @@
+// Imports the Google Cloud client library
+import { Storage } from '@google-cloud/storage';
+import type { RequestHandler } from '@sveltejs/kit';
+
+const bucketName = 'aiu-family-media';
+
+async function loadFileTestFile() {
+  // Creates a client
+  const storage = new Storage();
+  // Uploads a local file to the bucket
+  await storage.bucket(bucketName).upload('./README.md', {
+    // Support for HTTP requests made with `Accept-Encoding: gzip`
+    gzip: true,
+  });
+
+};
+
+loadFileTestFile().catch(console.error);
+
+export const GET: RequestHandler = async (event) => {
+  
+  return new Response('Hello world!');
+}
