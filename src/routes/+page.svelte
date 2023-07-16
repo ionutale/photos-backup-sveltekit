337c866837d6242ff9bf66635455d79ed93cf174
Ion Utale <utale.ion@gmail.com>
2023-07-16 10:15:29 +0200

    images to load info and preview

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,63 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
+	let photos: FileList;
+
+  function beautifySize(byte: number) {
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (byte === 0) return '0 Byte';
+    const i = Math.floor(Math.log(byte) / Math.log(1024));
+    return Math.round(byte / Math.pow(1024, i)) + ' ' + sizes[i];
+  }
+
 
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
 <form method="POST" action="?/upload" use:enhance>
 	<label for="photos">Choose a photos to upload</label>
-	<input type="file" id="photos" name="photos" accept="image/*" multiple/>
+	<input type="file" id="photos" name="photos" accept="image/*" bind:files={photos} multiple />
 	<button>Submit</button>
 </form>
+
+{#each photos || [] as photo, i}
+	<div>
+		<img src={URL.createObjectURL(photo)} alt="photo" width="100" height="100" />
+		<p>{i}-{photo.name} <span>{beautifySize(photo.size)}</span> </p>
+	</div>
+{/each}
+
+<style>
+	div {
+		display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-evenly;
+    margin: 1rem 0;
+	}
+
+	img {
+		display: block;
+	}
+
+	p {
+		text-align: center;
+    font-weight: 600;
+	}
+
+  span {
+    font-weight: 400;
+  }
+
+	form {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	label {
+		margin-bottom: 1rem;
+	}
+
+	input {
+		margin-bottom: 1rem;
+	}
+</style>
