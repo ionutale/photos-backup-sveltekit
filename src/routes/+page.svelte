0e98d758039daa443f2bf8b8cf2c52024762d9d8
Ion Utale <utale.ion@gmail.com>
2023-07-18 07:20:34 +0200

    style improvement

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,12 +23,16 @@
 <style>
 
 	section {
-		margin: 0;
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+		align-items: center;
+		gap: 0.1rem;
 	}
 	.image-card {
 		display: inline-block;
     margin: auto;
-    width: calc(100vw / 4);
+    width: calc(100vw / 4 - calc(0.1rem * 4));
     aspect-ratio: 1/1;
 	}
 	.image-card img {
